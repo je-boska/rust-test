7a751048365d0e738c77eff62ccbfc201364a22f
je-boska <joneirikboska@gmail.com>
2022-02-09 15:57:41 +0100

    Add shorthand conditional

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,11 +1,20 @@
 // Conditionals - Used to check condition..
 
 pub fn run() {
-    let age = 18;
+    let age: u8 = 20;
+    let check_id: bool = false;
+    let knows_person_of_age: bool = true;
 
     // If/Else
-
-    if age >= 21 {
+    if age >= 21 && check_id || knows_person_of_age {
         println!("Bartender: What would you like to drink?");
+    } else if age < 21 && check_id {
+        println!("Bartender: Sorry, you have to leave");
+    } else {
+        println!("Bartender: I'll need to see your ID");
     }
+
+    // Shorthand If
+    let is_of_age = if age >= 21 { true } else { false };
+    println!("Is Of Age: {}", is_of_age);
 }
